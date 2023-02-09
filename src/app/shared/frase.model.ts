f1a32c394f86f34fe5e94aed559f1bc6a9340cd9
Matheus <math.s.gomes@gmail.com>
2023-02-09 11:34:03 -0300

    Refatoração: simplificação, construindo os atributos da classe no construtor

--- src/app/shared/frase.model.ts
+++ src/app/shared/frase.model.ts
@@ -1,9 +1,4 @@
 export class Frase {
-  public fraseEng: string = '';
-  public frasePtBr: string = '';
 
-  constructor(fraseEng: string, frasePtBr: string) {
-    this.fraseEng = fraseEng;
-    this.frasePtBr = frasePtBr;
-  }
+  constructor(public fraseEng: string, public frasePtBr: string) { }
 }
