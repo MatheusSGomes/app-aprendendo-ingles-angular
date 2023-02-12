939e840b9aa96b77ce65aa6f5c5a91623f2b71e1
Matheus <math.s.gomes@gmail.com>
2023-02-11 22:53:20 -0300

    Lógica verificar de resposta

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -12,11 +12,31 @@ export class PainelComponent {
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
   public resposta!: string;
+  public rodada: number = 0;
+  public rodadaFrase!: Frase;
 
-  constructor() { console.log(FRASES); };
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada];
+  };
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
     console.log(this.resposta);
+  }
+
+  public verificarRespota(): void {
+    // verificar se a tradução está correta
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('Tradução correta');
+      
+      // trocar pergunta da rodada
+      this.rodada++;
+
+      // atualiza frase
+      this.rodadaFrase = this.frases[this.rodada];
+
+    } else {
+      alert('Tradução errada');
+    }
   }
 }
